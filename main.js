178a984e70eccf725514320f09beab1a0625ad75
lvingonzales <lvingonzales15@gmail.com>
2024-12-03 00:22:37 -0500

    Add open form and close form button

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 // 1rem = 16px, base all measurements off of this
 
 const CONTAINER = document.querySelector(".container");
+const BOOK_FORM = document.querySelector(".book-form");
+const ADD_BOOK = document.querySelector(".add-book");
+const CLOSE_BUTTONS = document.querySelectorAll(".close-button");
 
 const MY_LIBRARY = [];
+
+function initialize() {
+    BOOK_FORM.style.display = "none";
+
+    ADD_BOOK.addEventListener("click", () => {
+        BOOK_FORM.style.display = "grid";
+    })
+
+    CLOSE_BUTTONS.forEach(button => {
+        button.addEventListener ("click", () => {
+            if (button.value === "form") {
+                BOOK_FORM.style.display = "none";
+            }
+        })
+    });
+}
+
+initialize();
 
 function Book() {
     this.name = name;
@@ -15,4 +36,8 @@ function Book() {
 
 function addBookToLibrary () {
 
+}
+
+function bookDomCreation () {
+    
 }
