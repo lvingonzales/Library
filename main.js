444239a49414fa3a0899ecef31fed229d690a339
lvingonzales <lvingonzales15@gmail.com>
2024-12-03 03:35:30 -0500

    Add book covers and Book Info

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 // 1rem = 16px, base all measurements off of this
 
 const CONTAINER = document.querySelector(".container");
+const BOOKSHELF = document.querySelector(".bookshelf");
 const BOOK_FORM = document.querySelector(".book-form");
 const ADD_BOOK = document.querySelector(".add-book");
 const CLOSE_BUTTONS = document.querySelectorAll(".close-button");
 const SUBMIT_BOOK = document.querySelector("#submit-button")
-
 const MY_LIBRARY = [];
+
+let coverDiv = document.createElement("div");
+let coverParagraphs = new Array(5);
+
+let currentId = 0;
 
 function initialize() {
     //BOOK_FORM.style.display = "none";
@@ -24,6 +29,8 @@ function initialize() {
             }
         })
     });
+    MY_LIBRARY[currentId] = new Book(currentId, "Liam's Lullaby", "Liam Gonzales", "2024",  "Casual", "")
+    currentId++;
 }
 
 function submitBtnClick (event) {
@@ -33,19 +40,30 @@ function submitBtnClick (event) {
 
 initialize();
 
-function Book() {
+function Book(id, name, author, year, genre, description) {
+    this.id = id
     this.name = name;
-    this.picture = picture;
-    this.description = description;
-    this.year = year;
     this.author = author;
+    this.year = year;
     this.genre = genre;
+    this.description = description;
+    this.printCoverText = function () {
+        let coverInfo = Object.values(this);
+        console.log(coverInfo);
+        for (let i = 0; i < coverParagraphs.length; i++ ){
+            coverParagraphs[i] = document.createElement('p');
+            coverParagraphs[i].innerText = coverInfo[i];
+            coverDiv.appendChild(coverParagraphs[i])
+        }
+    }
 }
 
 function addBookToLibrary () {
+    MY_LIBRARY[0].printCoverText();
+    coverDiv.classList.add ("white-font", "book-cover");
+    //coverDiv.innerText = MY_LIBRARY[0].printCoverText();
 
+    BOOKSHELF.appendChild(coverDiv);
 }
 
-function bookDomCreation () {
-    
-}
+addBookToLibrary();
