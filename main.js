72aeff27d06536442661bebef18470061ab3356c
lvingonzales <lvingonzales15@gmail.com>
2024-12-02 23:48:30 -0500

    set up grid and add book button

---
+++ main.js
@@ -0,0 +1,18 @@
+// 1rem = 16px, base all measurements off of this
+
+const CONTAINER = document.querySelector(".container");
+
+const MY_LIBRARY = [];
+
+function Book() {
+    this.name = name;
+    this.picture = picture;
+    this.description = description;
+    this.year = year;
+    this.author = author;
+    this.genre = genre;
+}
+
+function addBookToLibrary () {
+
+}
